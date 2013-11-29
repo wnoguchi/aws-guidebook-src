4a3dc2040a0da5f7f2d7f4837d4558fca5a1db03
Wataru Noguchi <wnoguchi@unicast.ne.jp>
2013-11-29 12:58:32 +0900

    Listing Buckets.

---
+++ s3/list_buckets.php
@@ -0,0 +1,22 @@
+<?php
+
+error_reporting(E_ALL);
+
+// Composer
+require_once('vendor/autoload.php');
+// AWS
+require_once('awssecure.inc.php');
+
+require_once('include/book.inc.php');
+
+use Aws\S3\S3Client;
+
+$s3 = S3Client::factory($config);
+
+$blist = $s3->listBuckets();
+
+foreach ($blist['Buckets'] as $b)
+{
+  echo "${b['Name']}: created at ${b['CreationDate']}\n";
+}
+
