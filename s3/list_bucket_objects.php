f299045896164a4bb8a57dd94932626902f2e24d
Wataru Noguchi <wnoguchi@unicast.ne.jp>
2013-12-02 12:55:35 +0900

    list objects.

---
+++ s3/list_bucket_objects.php
@@ -0,0 +1,31 @@
+<?php
+
+error_reporting(E_ALL);
+
+// Composer
+require_once('vendor/autoload.php');
+// AWS
+require_once('awssecure.inc.php');
+
+require_once('include/book.inc.php');
+
+use Aws\S3\S3Client;
+
+$s3 = S3Client::factory($config);
+
+$result = $s3->listObjects(array (
+  'Bucket' => BOOK_BUCKET,
+));
+
+if ($result['Contents']) {
+  foreach ($result['Contents'] as $obj)
+  {
+  //  var_dump($obj);
+    echo $obj['Key'] . "\n";
+  }
+}
+else
+{
+  echo "No objects found in " . BOOK_BUCKET . "\n";
+}
+
