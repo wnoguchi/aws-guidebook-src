fb123d916951983c0f043df0a46c3e203bf9a4aa
Wataru Noguchi <wnoguchi.0727@gmail.com>
2013-12-06 00:20:07 +0900

    Define Empty function: list bucket objects.

--- s3/include/book.inc.php
+++ s3/include/book.inc.php
@@ -1,3 +1,12 @@
 <?php
 define('BOOK_BUCKET', 'sitepoint-aws-cloud-book-wnoguchi');
 
+
+/**
+ * List All Bucket Objects.
+ * Not Limited 1000 objects this function call.
+ */
+function getAllBucketObjects($bucket, $prefix = '')
+{
+  // TODO Implement procedure.
+}
