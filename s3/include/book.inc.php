0e3904beba3d1ed3ea103a4e691ab681684f3b57
Wataru Noguchi <wnoguchi.0727@gmail.com>
2013-12-07 16:12:41 +0900

    Using log4php for debug.

--- s3/include/book.inc.php
+++ s3/include/book.inc.php
@@ -1,18 +1,21 @@
 <?php
 define('BOOK_BUCKET', 'sitepoint-aws-cloud-book-wnoguchi');
 
-
 /**
  * List All Bucket Objects.
  * Not Limited 1000 objects this function call.
  */
 function getAllBucketObjects($client, $bucket, $prefix = '')
 {
+  global $log;
+
   $objects = array();
   $next = '';
 
   do
   {
+    $log->debug("API Request.");
+
     $result = $client->listObjects(array (
       'Bucket' => $bucket,
       'Marker' => urlencode($next),
@@ -31,10 +34,13 @@ function getAllBucketObjects($client, $bucket, $prefix = '')
     $isTruncated = $result['IsTruncated'];
 
     if ($isTruncated) {
+      $log->debug("is truncated");
       $next = $objects[count($objects) - 1]['Key'];
     }
 
   } while ($isTruncated);
+
+  $log->debug("Reach End of function.");
 
   return $objects;
 
--- s3/list_bucket_objects_page.php
+++ s3/list_bucket_objects_page.php
@@ -9,6 +9,8 @@ require_once('include/awssecure.inc.php');
 
 require_once('include/book.inc.php');
 
+$log = Logger::getLogger("list_bucket_objects_page");
+
 use Aws\S3\S3Client;
 
 $s3 = S3Client::factory($config);
@@ -18,10 +20,13 @@ $bucket_name = BOOK_BUCKET;
 $result = getAllBucketObjects($s3, $bucket_name);
 
 if (!empty($result)) {
+  echo "-----------------------------------------------------\n";
   foreach ($result as $obj)
   {
     echo $obj['Key'] . "\n";
   }
+  echo "-----------------------------------------------------\n";
+  echo count($result) . " items fetched.\n";
 }
 else
 {
