902b952d2c96c7167eaa7880c4c40fdf33d5e103
Wataru Noguchi <wnoguchi.0727@gmail.com>
2013-12-17 00:51:05 +0900

    Add JSON decoded example.

--- s3/json_test.php
+++ s3/json_test.php
@@ -6,7 +6,12 @@ $values = array(
   array(1, 2, array('a' => 'Uno', 'b' => 'Dos')),
 );
 
-echo json_encode($values) . "\n";
+$encoded_value = json_encode($values);
+
+echo $encoded_value . "\n";
 
 // Result:
 // [1,"one",[1,2,3],[1,2,{"a":"Uno","b":"Dos"}]]
+
+$decoded_value = json_decode($encoded_value, true);
+var_dump($decoded_value);
