fc30e6c8cbbe3a3bf85b6133cedd213c45051121
Wataru Noguchi <wnoguchi.0727@gmail.com>
2013-12-21 13:55:20 +0000

    Fix syntax error.

--- s3/include/awssecure.inc.php
+++ s3/include/awssecure.inc.php
@@ -2,6 +2,6 @@
 // AWSアクセストークン
 $config = array(
   'key'    => 'AKIAIOSFODNN7EXAMPLE',
-  'secret' => 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+  'secret' => 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
   'region' => \Aws\Common\Enum\Region::AP_NORTHEAST_1,
 );
